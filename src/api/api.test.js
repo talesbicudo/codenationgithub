fc5ae36aab837bbf9cfdac05d656bdd6c237d681
tales <tales.bicudo@gmail.com>
2019-05-13 18:08:25 -0300

    has corrected instance test description ortography

--- src/api/api.test.js
+++ src/api/api.test.js
@@ -2,8 +2,9 @@ import { client } from '../api';
 import {ApolloClient} from 'apollo-client';
 
 describe("API testing", () => {
-    it('Creates a valide Apollo Client', () => {
+    it('Creates a valid Apollo Client', () => {
         expect(client).toBeInstanceOf(ApolloClient);
     })
+
 
 })
