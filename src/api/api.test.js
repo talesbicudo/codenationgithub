5bb7e5ce162a5f1e793d1c29e97ed613b4203b2b
tales <tales.bicudo@gmail.com>
2019-05-14 09:53:20 -0300

    Client has valid graphql requests

--- src/api/api.test.js
+++ src/api/api.test.js
@@ -1,10 +1,25 @@
 import { client } from '../api';
-import {ApolloClient} from 'apollo-client';
+import gql  from 'graphql-tag';
+import { ApolloClient } from 'apollo-client';
 
 describe("API testing", () => {
     it('Creates a valid Apollo Client', () => {
         expect(client).toBeInstanceOf(ApolloClient);
     })
 
+    it('Send valid graphql queries', async () => {
+        const query = gql`
+            query {
+                user(login: "talesbicudo"){
+                    login
+                }
+            } `
+        const data = await client.query({ query })
+            .then(data => data)
+            .catch(error => error)
+
+            console.log(data)
+        expect(data).not.toBeInstanceOf(Error);
+    })
 
 })
--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,24 @@
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link'
 import { InMemoryCache } from 'apollo-cache-inmemory';
-const ENDPOINT_URI = process.env.REACT_API_ENDPOINT;
+
+const
+    ENDPOINT_URI = process.env.REACT_APP_API_ENDPOINT,
+    ENDPOINT_TOKEN = process.env.REACT_APP_API_TOKEN
+
 const cache = new InMemoryCache();
-const link = createHttpLink({ uri: ENDPOINT_URI })
+const httpLink = createHttpLink({ uri: ENDPOINT_URI })
+
+const authMiddleware = new ApolloLink((operation, foward) => {
+    operation.setContext(context => ({
+        headers: {
+            ...context.headers,
+            authorization: `Bearer ${ENDPOINT_TOKEN}`
+        }
+    }))
+    return foward(operation);
+})
+
+const link = authMiddleware.concat(httpLink);
 export const client = new ApolloClient({ cache, link });
