fe3f297407edf6e9c031820be82d9c710703bf27
tales <tales.bicudo@gmail.com>
2019-05-15 10:02:41 -0300

    Has removed ApollLink import on client

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,5 @@
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
-import { ApolloLink } from 'apollo-link'
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const
