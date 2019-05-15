49758cda0f0a188334ddd08e2a05f76ab41fed4f
tales <tales.bicudo@gmail.com>
2019-05-15 10:01:55 -0300

    Has simplified link definition

--- src/api/api.test.js
+++ src/api/api.test.js
@@ -1,24 +1,34 @@
 import { client } from './client';
-import gql  from 'graphql-tag';
+import gql from 'graphql-tag';
 import { ApolloClient } from 'apollo-client';
+import { createApi } from '../api';
 
 describe("API testing", () => {
-    it('Creates a valid Apollo Client', () => {
-        expect(client).toBeInstanceOf(ApolloClient);
-    })
 
-    it('Send valid graphql queries', async () => {
-        const query = gql`
+    const Api = createApi({commitsPerPage: 10});
+    const testlogin = "andrew";
+
+        it('Creates a valid Apollo Client', () => {
+            expect(client).toBeInstanceOf(ApolloClient);
+        })
+
+        it('Send valid graphql queries', async () => {
+            const query = gql`
             query {
                 user(login: "talesbicudo"){
                     login
                 }
             } `
-        const data = await client.query({ query })
-            .then(data => data)
-            .catch(error => error)
+            const data = await client.query({ query })
+                .then(data => data)
+                .catch(error => error)
 
-        expect(data).not.toBeInstanceOf(Error);
+            expect(data).not.toBeInstanceOf(Error);
+        })
+
+        it('Retrieve commit by a given username', async () => {
+            const data = await Api.userRepositories(testlogin);
+            expect(data).not.toBeInstanceOf(Error);
+        })
+
     })
-
-})
--- src/api/client.js
+++ src/api/client.js
@@ -8,17 +8,6 @@ const
     ENDPOINT_TOKEN = process.env.REACT_APP_API_TOKEN
 
 const cache = new InMemoryCache();
-const httpLink = createHttpLink({ uri: ENDPOINT_URI })
+const link = createHttpLink({ uri: ENDPOINT_URI, headers: { authorization: `Bearer ${ENDPOINT_TOKEN}` } })
 
-const authMiddleware = new ApolloLink((operation, foward) => {
-    operation.setContext(context => ({
-        headers: {
-            ...context.headers,
-            authorization: `Bearer ${ENDPOINT_TOKEN}`
-        }
-    }))
-    return foward(operation);
-})
-
-const link = authMiddleware.concat(httpLink);
 export const client = new ApolloClient({ cache, link });
