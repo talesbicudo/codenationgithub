5fb2407e418ef1b7bf4a67d67a65346507f0c63a
tales <tales.bicudo@gmail.com>
2019-05-24 07:44:42 -0300

    Has add SearchInput saga fork

--- src/redux/rootSagas.js
+++ src/redux/rootSagas.js
@@ -1,5 +1,10 @@
+import SearchInput from './SearchInput/sagas'
+
 /* Effects */
+import { fork } from 'redux-saga/effects'
+
+
 
 export default function* rootSagas() {
-  
+  yield fork(SearchInput);
 }
