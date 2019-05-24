836844194c444ea5db63c2c6cbb00de50d20bb35
tales <tales.bicudo@gmail.com>
2019-05-24 10:49:35 -0300

    Link to each item on searchlist

--- src/components/SearchList/index.js
+++ src/components/SearchList/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import SearchListItem from '../SearchListItem';
 
-const SearchList = ({ items }) => (
-    <div style={{position: 'absolute'}}>
-        {items.map((item) => {
-            return <SearchListItem key={item.id} {...item} />
-        })}
-    </div>
-)
+const SearchList = ({ items, createLink }) => {
+    const innerCreateLink = item => createLink(item);
+    return (
+        <div style={{ position: 'absolute' }}>
+            {items.map((item) => {
+                return <Link key={item.id} to={innerCreateLink(item)}><SearchListItem key={item.id} {...item} /></Link>
+            })}
+        </div>
+    )
+}
+
 
 
 
