70442cb57bae97c5b0350f4051b9bb9c74119b2f
tales <tales.bicudo@gmail.com>
2019-05-24 13:00:25 -0300

    onLoaded for searchList

--- src/components/SearchList/index.js
+++ src/components/SearchList/index.js
@@ -2,12 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import SearchListItem from '../SearchListItem';
 
-const SearchList = ({ items, createLink }) => {
+const SearchList = ({ items, createLink, onLoaded }) => {
     const innerCreateLink = item => createLink(item);
+
+    const loadingItems = function*() {
+        for(let i = 0; i < items.length - 1; i++){
+            yield null;
+        }
+        yield onLoaded();
+    }
+
+    const loadingItemsHandler = loadingItems();
     return (
         <div style={{ position: 'absolute' }}>
             {items.map((item) => {
-                return <Link key={item.id} to={innerCreateLink(item)}><SearchListItem key={item.id} {...item} /></Link>
+                return <Link key={item.id} to={innerCreateLink(item)}>
+                    <SearchListItem onLoaded={() => loadingItemsHandler.next()}
+                        key={item.id} {...item} />
+                </Link>
             })}
         </div>
     )
--- src/components/SearchListItem/index.js
+++ src/components/SearchListItem/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import useNodeWithId from '../../QueryHooks/useNodeWithId';
 import typeMaps from './typeMaps';
 
-const SearchListItem = ({ id, type }) => (
-    <div style={{backgroundColor: 'white', width: "30vw", margin: "0", outline: "solid black 0.1rem"}}>
-        {useNodeWithId({ id, type, ...typeMaps[type] })}
-    </div>
-)
-
+const SearchListItem = ({ id, type, onLoaded }) => {
+    const typeMap = typeMaps[type];
+    const nodeWithIdprops = { ...typeMap, onLoaded: onLoaded };
+    return (
+        <div style={{ backgroundColor: 'white', width: "30vw", margin: "0", outline: "solid black 0.1rem" }}>
+            {useNodeWithId({ id, type, ...nodeWithIdprops})}
+        </div>
+    )
+}
 export default SearchListItem;
