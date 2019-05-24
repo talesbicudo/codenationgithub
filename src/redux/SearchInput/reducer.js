55a5ac2af4af13ad4ae7ce3b96a71986521333c3
tales <tales.bicudo@gmail.com>
2019-05-24 07:45:46 -0300

    Has add SUCCESS handle on Search reducer

--- src/redux/SearchInput/reducer.js
+++ src/redux/SearchInput/reducer.js
@@ -1,3 +1,10 @@
-export default (store = {searchText: "", visibility: false, loading: false}) => {
-    return store
+import ActionTypes from './ActionTypes';
+export default (store = {searchText: "", visibility: false, loading: false}, action) => {
+   const {type, payload} = action;
+   switch(type){
+       case ActionTypes.SUCCESS:
+            return {...store, visibility: true, loading: false};
+       default:
+            return store;
+   }
 }
