f7cdfbf0beb080665d5de4a3c681b2fbe7512844
tales <tales.bicudo@gmail.com>
2019-05-26 19:34:32 -0300

    Batch link if on context

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,8 @@
 
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
-import { ApolloLink, concat } from 'apollo-link';
+import { BatchHttpLink } from 'apollo-link-batch-http';
+import { ApolloLink, concat, split } from 'apollo-link';
 import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import introspectionQueryResultData from './fragmentTypes.json';
 import { getAuthToken } from '../services/login';
@@ -10,6 +11,7 @@ import { getAuthToken } from '../services/login';
 export const ENDPOINT_URI = "https://api.github.com/graphql"
 
 const httpLink = createHttpLink({ uri: ENDPOINT_URI })
+const batchHttpLink = new BatchHttpLink({uri: ENDPOINT_URI});
 
 const authLink = new ApolloLink((operation, forward) => {
     operation.setContext(context => ({
@@ -21,12 +23,14 @@ const authLink = new ApolloLink((operation, forward) => {
     return forward(operation)
 })
 
-const link = concat(authLink, httpLink);
+const usedHttpLink = split(({getContext}) => getContext().batch, batchHttpLink, httpLink);
+
+const link = concat(authLink, usedHttpLink);
 const fragmentMatcher = new IntrospectionFragmentMatcher({
     introspectionQueryResultData
-  });
+});
 
-const cache = new InMemoryCache({fragmentMatcher});
+const cache = new InMemoryCache({ fragmentMatcher });
 
 const client = new ApolloClient({ cache, link });
 
