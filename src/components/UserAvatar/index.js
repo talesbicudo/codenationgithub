77e6334e4bad25133e7815b2cca503911e2dd477
fabcall <43019077+fabcall@users.noreply.github.com>
2019-05-27 10:59:08 -0300

    Added propTypes

--- src/components/UserAvatar/index.js
+++ src/components/UserAvatar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -19,6 +20,12 @@ const LanguageAvatar = ({ name, big, avatarUrl }) => {
     return (
         <Avatar className={`${big ? classes.bigAvatar : ''} ${classes.avatar}`} alt={name} src={avatarUrl} />
     )
+}
+
+LanguageAvatar.propTypes = {
+    name: PropTypes.string,
+    big: PropTypes.bool,
+    avatarUrl: PropTypes.string
 }
 
 export default LanguageAvatar;
