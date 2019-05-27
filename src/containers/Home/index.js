97db7fbef3727eb7aaf7c9efd6fd8a6a70c88ce7
tales <tales.bicudo@gmail.com>
2019-05-26 23:48:18 -0300

    Container replaces div

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,9 +3,13 @@ import { jsx, css } from '@emotion/core';
 import MainSearch from '../MainSearch';
 import Profile from '../Profile';
 import Nav from '../../components/Nav'
+import Container from '@material-ui/core/Container';
+import useRepositoriesByDateInterval from '../../QueryHooks/useRepositoriesByDateInteval';
+
 export const Home = ({ type = 'User', name = "" }) => {
+    
     return (
-        <div className="home" css={css`
+        <Container className="home" css={css`
             > * {
                 padding: .5rem 2rem;
             } 
@@ -19,7 +23,7 @@ export const Home = ({ type = 'User', name = "" }) => {
                 </div>
             </Nav>
             <Profile type={type} name={name} />
-        </div>
+        </Container>
     )
 }
 
