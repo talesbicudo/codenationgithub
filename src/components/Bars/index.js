3011afee1d32d292c13e4f973e78caa804a12263
tales <tales.bicudo@gmail.com>
2019-05-28 09:46:02 -0300

    Bars styling

--- src/components/Bars/index.js
+++ src/components/Bars/index.js
@@ -1,39 +1,41 @@
 import React from 'react';
+
 import { ResponsiveBar } from '@nivo/bar';
-
 function fnum(x) {
-	if(isNaN(x)) return x;
+    if (isNaN(x)) return x;
 
-	if(x < 9999) {
-		return x;
-	}
+    if (x < 9999) {
+        return x;
+    }
 
-	if(x < 1000000) {
-		return Math.round(x/1000) + "K";
-	}
-	if( x < 10000000) {
-		return (x/1000000).toFixed(2) + "M";
-	}
+    if (x < 1000000) {
+        return Math.round(x / 1000) + "K";
+    }
+    if (x < 10000000) {
+        return (x / 1000000).toFixed(2) + "M";
+    }
 
-	if(x < 1000000000) {
-		return Math.round((x/1000000)) + "M";
-	}
+    if (x < 1000000000) {
+        return Math.round((x / 1000000)) + "M";
+    }
 
-	if(x < 1000000000000) {
-		return Math.round((x/1000000000)) + "B";
-	}
+    if (x < 1000000000000) {
+        return Math.round((x / 1000000000)) + "B";
+    }
 
-	return "1T+";
+    return "1T+";
 }
 
-const Bars = ({ data, legend, onClick }) => (
+const Bars = ({ legend, ...props }) => (
     <ResponsiveBar
-        data={data}
-        keys={['Total']}
+        {...props}
+       keys={['Total']}
         indexBy={'item'}
         margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
         padding={0.3}
-        colors={{ scheme: 'orange_red' }}
+        colors={["#61B0CF"]}
+        labelTextColor={{ from: 'color', modifiers: [['brighter', '3']] }}
+        layout="horizontal" enableGridY={false} enableGridX={true}
         borderColor={{ from: 'color', modifiers: [['darker', '0.1']] }}
         axisTop={null}
         axisRight={null}
@@ -41,16 +43,16 @@ const Bars = ({ data, legend, onClick }) => (
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: legend,
+            legend: "Repositórios",
             legendPosition: 'middle',
-            legendOffset: 32
+            legendOffset: 32,
+            format: v => fnum(v)
         }}
         axisLeft={{
-            format: v => fnum(v),
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: "Repositórios",
+            legend,
             legendPosition: 'middle',
             legendOffset: -50
         }}
@@ -59,7 +61,22 @@ const Bars = ({ data, legend, onClick }) => (
         animate={true}
         motionStiffness={90}
         motionDamping={15}
-        onClick={onClick}
+        isInteractive
+        tooltip={({ indexValue, color }) => (
+            <strong style={{ color }}>
+                Detalhes: {legend} {indexValue}
+            </strong>
+        )}
+        theme={{
+            tooltip: {
+                container: {
+                    background: '#333',
+                },
+            },
+        }}
+        onMouseEnter={(data, event) => {
+            event.target.style.cursor = "pointer";
+        }}
     />
 )
 export default Bars; 
