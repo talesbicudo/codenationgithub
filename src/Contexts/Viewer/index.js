5d299d8340cf7cf3d338f9bec4e9c5bbe62598fb
tales <tales.bicudo@gmail.com>
2019-05-28 15:30:18 -0300

    name to login as default context

--- src/Contexts/Viewer/index.js
+++ src/Contexts/Viewer/index.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
-const Viewer = createContext({name: "", avatarUrl: ""});
+const Viewer = createContext({login: "", avatarUrl: ""});
 
 export default Viewer;
