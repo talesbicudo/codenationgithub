ba54b99f216088b0f7e754610f1a33399c07b220
tales <tales.bicudo@gmail.com>
2019-05-29 13:51:27 -0300

    Get public and fork repositories

--- src/containers/Profile/UserHeader.js
+++ src/containers/Profile/UserHeader.js
@@ -1,26 +1,28 @@
 import React from 'react'
-import useUserWithLogin from '../../QueryHooks/useUserWithLogin';
+import useRepositoriesWithSearch from '../../QueryHooks/useRepositoriesWithSearch';
 import Header from '../../components/Header';
 import UserAvatar from '../../components/UserAvatar';
 
 const UserHeader = ({ name }) => {
 
     const fetchProps = `
-        avatarUrl
-        login
-        repositories(first: 1) {
-            totalCount
-        }
+        owner {
+            login
+            avatarUrl
+       } 
     `
-    const { loading, user } = useUserWithLogin({ login: name, fetchProps })
+    const { loading, repositories, totalCount } = useRepositoriesWithSearch({ search: `is:public fork:true user:${name}`, fetchProps })
+
 
     if (loading) return null;
+
+    const user = repositories[0].owner;
 
     return (
         <Header
             avatar={<UserAvatar big alt={user.login} avatarUrl={user.avatarUrl} />}
             name={user.login}
-            totalCount={user.repositories.totalCount}
+            totalCount={totalCount}
         />
 
     )
