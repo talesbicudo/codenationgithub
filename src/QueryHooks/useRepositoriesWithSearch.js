1d91d7594392e1daf9e9cc118bd64a67f581acf1
tales <tales.bicudo@gmail.com>
2019-05-29 15:36:15 -0300

    formating and cleanup

--- src/QueryHooks/useRepositoriesWithSearch.js
+++ src/QueryHooks/useRepositoriesWithSearch.js
@@ -2,11 +2,30 @@ import gql from 'graphql-tag';
 import useCheckedQuery from './useCheckedQuery';
 
 
-const mapDataToProps = ({search}) => ({
+const mapDataToProps = ({ search }) => ({
     repositories: search.nodes,
     totalCount: search.repositoryCount,
     ...search.pageInfo
 })
+
+
+const getPageInfo = data => data.search.pageInfo;
+
+const fetchMoreMoreHandler = (prev, next) => {
+    console.log('hi');
+    const prevNodes = prev.search.nodes,
+        fetchNodes = next.search.nodes,
+        pageInfo = prev.search.pageInfo;
+    return {
+        search: {
+            __typename: prev.search.__typename,
+            repositoryCount: prev.search.repositoryCount,
+            pageInfo,
+            nodes: [...prevNodes, ...fetchNodes]
+        }
+    }
+
+}
 
 const useRepositoriesWithSearch = ({
     fetchProps = "name",
@@ -34,7 +53,8 @@ const useRepositoriesWithSearch = ({
             }
         }
         ${fragment}`
-    return useCheckedQuery(query, mapDataToProps, {variables: {search, itemsPerPage, cursor: null}})
+    return useCheckedQuery(query, mapDataToProps,
+        { variables: { search, itemsPerPage, cursor: null } }, { getPageInfo, fetchMoreMoreHandler })
 }
 
 export default useRepositoriesWithSearch;
