ba1be860636ea71c7846b086efd7b6d7ae2b9d75
tales <tales.bicudo@gmail.com>
2019-05-29 15:46:07 -0300

    Box Styling

--- src/containers/RepositoryData/RepositoryCalendar.js
+++ src/containers/RepositoryData/RepositoryCalendar.js
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import Calendar from '../../components/Calendar';
 import { gitIsoDate } from '../../redux/RepositoryData/reducer';
-
+import Box from '@material-ui/core/Box';
 const RepositoryCalendar = ({ data, dispatch }) => {
     const usableData
         = useMemo(() => {
@@ -14,7 +14,11 @@ const RepositoryCalendar = ({ data, dispatch }) => {
         }, [data]);
 
 
-    return <Calendar {...usableData} />
+    return (
+        <Box display="flex" alignItems="center" height="65vh" width="100%">
+            <Calendar {...usableData} />
+        </Box>
+    )
 }
 
 export default RepositoryCalendar;
