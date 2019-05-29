fb28de0c4f0ac6d01c789b058cf23c933a001b75
tales <tales.bicudo@gmail.com>
2019-05-29 16:02:24 -0300

    Back Button test

---
+++ src/components/BackButton/BackButton.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import BackButton from './index';
+import { render } from 'react-testing-library';
+describe('Back buttonj', () => {
+    it('renders', () => {
+        const { getByText } = render(<BackButton/>);
+        expect(getByText('Voltar')).toBeTruthy()
+    })
+})
